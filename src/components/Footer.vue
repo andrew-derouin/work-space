2639650ef4b374ef4d42a3062d1df04e8c12a549
Andrew Derouin <andrew.derouin@deckers.com>
2020-09-25 22:42:30 -0700

    AD - add footer icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,18 @@
 <!-- src/components/Main.vue -->
 <template>
     <div class="footer-element">
-        
+        <div class="icon-row">
+            <a href="https://www.linkedin.com/in/andrew-derouin-b28277197/" title="Link to Andrew DeRouin's Linkedin account." target="_blank">
+                <div class="icon icon-linkedin">
+                    <img v-bind:src="'./src/icons/linkedin-icon.svg'" />
+                </div>
+            </a>
+            <a href="https://bitbucket.org/andrew--derouin/work-place/src/master/" title="Link to the Bitbucket Repository." target="_blank">
+                <div class="icon icon-bitbucket">
+                    <img v-bind:src="'./src/icons/bitbucket-icon.svg'" />
+                </div>
+            </a>
+        </div>
     </div>
 </template>
 

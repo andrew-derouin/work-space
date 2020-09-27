f941cf1a6094de510a6e718970b2f5e8b668bcca
Andrew Derouin <andrew.derouin@deckers.com>
2020-09-27 00:32:18 -0700

    AD - add some alt text

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -8,10 +8,10 @@
             </div>
             <div class="screenshot-case">
                 <div class="screenshot-element">
-                    <img v-bind:src="itemData.images[1]" />
+                    <img v-bind:src="itemData.images[1]" alt="This is a screenshot from {{itemData.name}}." />
                 </div>
                 <div class="screenshot-element">
-                    <img v-bind:src="itemData.images[2]" />
+                    <img v-bind:src="itemData.images[2]" al="This is another screenshot from {{itemData.name}}." />
                 </div>
             </div>
         </div>
